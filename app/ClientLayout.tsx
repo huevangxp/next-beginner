34bab45bbdc83d75a0aeb43ed69f4580219df61e
huevangxp <huevang933@gmail.com>
2025-12-18 09:52:01 +0700

    commit Thu 12/18/2025  9:52:01.15

---
+++ app/ClientLayout.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
+
+export default function ClientLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const pathname = usePathname();
+  const isLoginPage = pathname === "/login";
+
+  if (isLoginPage) {
+    return <main>{children}</main>;
+  }
+
+  return (
+    <div className="flex min-h-screen">
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* Main Content */}
+      <div className="flex-1 flex flex-col">
+        <Header />
+        <main className="p-8 flex-1">
+          <div className="max-w-7xl mx-auto">{children}</div>
+        </main>
+      </div>
+    </div>
+  );
+}
