b3a6ca82f67be5e6651580cb3e627d67dbc28c16
huevangxp <huevang933@gmail.com>
2025-12-18 13:48:42 +0700

    commit Thu 12/18/2025 13:48:42.17

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -61,7 +61,7 @@ const LoginPage = () => {
         {/* Left Side: Illustration */}
         <div className="hidden md:flex md:w-1/2 relative items-center justify-center overflow-hidden border-r border-gray-50 dark:border-gray-800">
           <Image
-            src="auth.png"
+            src="/auth.png"
             alt="Auth Illustration"
             width={500}
             height={500}
