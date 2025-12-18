63fafcb5789ba3c7318eca9d0921ae30cee66ed8
huevangxp <huevang933@gmail.com>
2025-12-18 14:09:50 +0700

    commit Thu 12/18/2025 14:09:50.20

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [], // if using external images
+  },
 };
 
 export default nextConfig;
