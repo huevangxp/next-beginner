1bf1998242179dc8527714d79eeb93e177ac7c0b
huevangxp <huevang933@gmail.com>
2025-12-18 14:11:44 +0700

    commit Thu 12/18/2025 14:11:44.20

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -47,7 +47,7 @@ const RegisterPage = () => {
   return (
     <div className="min-h-screen bg-white dark:bg-gray-950 flex items-center justify-center p-0 md:p-6 relative overflow-hidden">
       {/* Background Decorative Elements */}
-      <img src="/auth.png" alt="" />
+      <img src="/auth.png" alt="test" />
       <div className="absolute top-0 left-0 w-full h-full overflow-hidden pointer-events-none">
         <div className="absolute top-[-10%] left-[-10%] w-[40%] h-[40%] bg-blue-500/5 rounded-full blur-[120px]"></div>
         <div className="absolute bottom-[-10%] right-[-10%] w-[40%] h-[40%] bg-teal-500/5 rounded-full blur-[120px]"></div>
