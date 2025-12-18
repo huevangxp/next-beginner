89f1b88f3ebd520f97e4c770c2e341451bb81ad5
huevangxp <huevang933@gmail.com>
2025-12-18 14:05:00 +0700

    commit Thu 12/18/2025 14:05:00.12

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -59,7 +59,7 @@ const RegisterPage = () => {
 
       <div className="w-full max-w-6xl bg-white dark:bg-gray-900 rounded-none md:rounded-[40px] shadow-2xl border border-gray-100 dark:border-gray-800 overflow-hidden flex flex-col md:flex-row-reverse min-h-[700px] relative z-10">
         {/* Right Side: Illustration */}
-        <div className="hidden md:flex md:w-1/2 relative items-center justify-center overflow-hidden border-l border-gray-50 dark:border-gray-800">
+        <div className=" md:flex md:w-1/2 relative items-center justify-center overflow-hidden border-l border-gray-50 dark:border-gray-800">
           <Image
             src="/assets/images/auth.png"
             alt="Auth Illustration"
